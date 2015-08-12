e2de848ea2c9b91f1a0bc9672d7b1bb231d759bf
David <chou.david88@gmail.com>
2015-08-12 00:57:22 -0700

    Changes to Structure and CSS

--- content.php
+++ content.php
@@ -16,46 +16,36 @@ if ( has_post_thumbnail() ) {
 </div> <!-- left-content-->
 <div class="right-content">
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-		<!--<h1 class="entry-title"><?php the_title(); ?></h1>-->
+		<!--<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>-->
+		<h1 class="entry-title"><?php the_title(); ?></h1>
 <?php
 	/* Assign variables with custom fields */
 	$postid = get_the_ID();
 	$location = get_post_meta( $postid , "Location", true); 
 	$date = get_post_meta( $postid , "Date", true); 
 	$tickets = get_post_meta( $postid , "Buy Tickets", true); 
+	$venue  = get_post_meta( $postid , "Venue", true); 
+	$page_url = esc_url( get_permalink() );
 ?>
 <!--<?php echo $date ?>
 <?php the_meta(); ?>-->
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta"> <br />
-			<p><span class="Span_Location"> <?php echo $location ?> </span></p> <!-- Style This -->
-			<p><span class="Span_Tickets"><a href="<?php echo $tickets ?>"> Buy Tickets </a> </span></p><!-- Style This -->
+			<p class="location"><span class="Span_Venue"> <?php echo $venue ?> </span> <br>
+			<span class="Span_Location"> <?php echo $location ?> </span></p> <!-- Style This -->
+			
 			
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-	
-		<?php
-			
-			the_excerpt( sprintf(
-				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'pacify' ), 
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-		?>
-<!--
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'pacify' ),
-				'after'  => '</div>',
-			) );
-		?>-->
+
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
-		<?php //pacify_entry_footer(); ?>
+	<p><a href="<?php echo $tickets ?>"><input type="button" id="Button_Ticket" value="Buy Tickets" class="Button_Tickets"></a><!-- Style This -->
+			<a href="<?php echo $page_url ?>"><input type="button" id="Button_Information" value="More Information" class="Button_Information"></a></p>
 	</footer><!-- .entry-footer -->
 	
 	</div> <!-- right-content -->
